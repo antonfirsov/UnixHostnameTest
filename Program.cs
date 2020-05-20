bb16c12c1d308c5f10c6848e6127f95e7d288f21
Anton Firszov <antonfir@gmail.com>
2020-05-20 18:46:44 +0200

    test

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace UnixHostnameTest
 {
@@ -6,7 +8,44 @@ namespace UnixHostnameTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string hostName = GetHostName();
+            Console.WriteLine(hostName);
+
+            IntPtr ptr = gethostbyname(hostName);
+            bool success = ptr != IntPtr.Zero;
+            
+            Console.WriteLine("gethostbyname succeeded: "+success);
+        }
+        
+        [DllImport("libc")]
+        private static extern IntPtr gethostbyname(string name);
+
+
+        [DllImport("System.Native", EntryPoint = "SystemNative_GetHostName", SetLastError = true)]
+        private static extern unsafe int GetHostName(byte* name, int nameLength);
+
+        internal static unsafe string GetHostName()
+        {
+            const int HOST_NAME_MAX = 255;
+            const int ArrLength = HOST_NAME_MAX + 1;
+
+            byte* name = stackalloc byte[ArrLength];
+            int err = GetHostName(name, ArrLength);
+            if (err != 0)
+            {
+                // This should never happen.  According to the man page,
+                // the only possible errno for gethostname is ENAMETOOLONG,
+                // which should only happen if the buffer we supply isn't big
+                // enough, and we're using a buffer size that the man page
+                // says is the max for POSIX (and larger than the max for Linux).
+                Debug.Fail($"GetHostName failed with error {err}");
+                throw new InvalidOperationException($"{nameof(GetHostName)}: {err}");
+            }
+
+            // If the hostname is truncated, it is unspecified whether the returned buffer includes a terminating null byte.
+            name[ArrLength - 1] = 0;
+
+            return Marshal.PtrToStringAnsi((IntPtr)name)!;
         }
     }
 }
